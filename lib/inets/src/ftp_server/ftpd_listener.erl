a9ac4ae07b99e6665ef3733ec5bc8a9e2fe787ca
Hegedus Denes <denes.heged.s@ericsson.com>
2012-06-22 13:02:20 +0200

    Document and cleanup message handling

--- lib/inets/src/ftp_server/ftpd_listener.erl
+++ lib/inets/src/ftp_server/ftpd_listener.erl
@@ -17,6 +17,7 @@ loop(LSock, SupPid, Args) ->
 
 new_connection(Sock, SupPid, Args) ->
 	erlang:monitor(process, SupPid),
+	io:format("---------------- CONNECTION START ----------------\n"),
 	send_reply(Sock, 220, "hello"),
 	do_recv(Sock, Args).
 
@@ -64,14 +65,18 @@ code_change(_OldVsn, State, _Extra) -> {ok, State}.
 %% FTP control connection for handling commands
 %%
 
+-type proplist()   :: list().
+-type reply()      :: {reply, ReplyCode :: integer(), Message :: string()}.
+-type argschange() :: sameargs | {newargs, NewArgs :: proplist()}.
+
 %% Wait for incoming messages
 do_recv(Sock, Args) ->
     case gen_tcp:recv(Sock, 0) of
         {ok, Data} ->
-			io:format("Data received: ~p \n",[Data]),
-			DataStr = binary_to_list(Data),
+			DataStr        = binary_to_list(Data),
 			{Command, Msg} = packet_to_tokens(DataStr),
-			io:format("Command: ~p, Msg: ~p\n",[Command, Msg]),
+			io:format("[Recv]: ~p - ~p\n", [Command, Msg]),
+
 			{Reply, MaybeNewArgs} = handle_message(Command, Msg, Args),
 			handle_reply(Sock, Reply),
 			NewArgs = case MaybeNewArgs of
@@ -82,7 +87,8 @@ do_recv(Sock, Args) ->
         {error, closed} -> ok
     end.
 
-%% Separate command from message
+%% Separate command from message and convert to upper case, eg. "user someone" -> {"USER", ["someone"]}
+-spec packet_to_tokens(Data :: string()) -> {Command :: string(), [Message :: string()]}.
 packet_to_tokens(Data) ->
 	TrimmedData = string:strip(string:strip(Data, right, ?LF), right, ?CR),
 	case string:str(TrimmedData, " ") of
@@ -90,32 +96,44 @@ packet_to_tokens(Data) ->
 		Len ->
 			Command = string:to_upper(string:sub_string(TrimmedData, 1, Len - 1)),
 			Msg     = string:sub_string(TrimmedData, length(Command) + 2),
-    		{Command, Msg}
+    		{Command, string:tokens(Msg, " ")}
 	end.
 
+%% Send response if needed
 handle_reply(_, noreply) ->
 	ok;
 handle_reply(Sock, {reply, Code, Message}) ->
 	send_reply(Sock, Code, Message).
 
+%% Convert Code and Message to 
 send_reply(Sock, Code, Message) ->
+	io:format("[Send]: ~p - ~p\n", [Code, Message]),
 	Str = integer_to_list(Code) ++ " " ++ Message ++ "\r\n",
 	gen_tcp:send(Sock, Str).
 
-response(Command, Message) -> {reply, Command, Message}.
+%% Construct tuple for response
+-spec response(ReplyCode :: integer(), Message :: string()) -> reply().
+response(ReplyCode, Message) -> {reply, ReplyCode, Message}.
 
+%% Handle incoming FTP commands
+-spec handle_message(Command :: string(), Message :: list(), Args :: proplist()) -> {reply(), argschange()}.
 handle_message("NOOP", _, _) ->
 	{response(200, "NOOP command successful"), sameargs};
 
 handle_message("QUIT", _, _) ->
 	{response(221, "Goodbye."), sameargs};
 
-handle_message("USER", User, Args) ->
-	NewArgs1 = proplists:delete(username, Args),
-	NewArgs2 = [ {username, User} | NewArgs1],
-	{response(331, "Password required for " ++ User), {newargs, NewArgs2}};
+handle_message("USER", [User|_], Args) ->
+  case proplists:get_bool(authed,Args) of
+    true -> 
+      {response(503, "You are already logged in"), sameargs};
+    false ->
+      NewArgs1 = proplists:delete(username, Args),
+      NewArgs2 = [ {username, User} | NewArgs1],
+      {response(331, "Password required for " ++ User), {newargs, NewArgs2}}
+  end;
 
-handle_message("PASS", Password, Args) ->
+handle_message("PASS", [Password|_], Args) ->
 	case {proplists:lookup(username, Args), proplists:get_value(pwd_fun, Args, fun(Uname,Pass) -> not_authorized end)} of
 		{{username, User}, PwdFun} ->
 			case PwdFun(User, Password) of
@@ -126,10 +144,22 @@ handle_message("PASS", Password, Args) ->
 			{response(503, "Login with USER first"), sameargs}
 	end;
 
+handle_message("TYPE", Type, Args) ->
+	NewArgs = [{repr_type, Type} | Args],
+	{response(200, "not yet"), {newargs, NewArgs}};
+
+handle_message("", _, _) ->
+	{response(500, "Invalid command: try being more creative"), sameargs};
+
 handle_message(Command, _, _) ->
 	{response(500, Command ++ " not implemented"), sameargs}.
 
+%% Control behaviour after replying to a message
 after_reply(Sock, "QUIT", _) ->
-	gen_tcp:close(Sock);
+	io:format("---------------- CONNECTION CLOSE ----------------\n"),
+	close_connection(Sock);
 after_reply(Sock, _, Args) ->
 	do_recv(Sock, Args).
+
+close_connection(Sock) ->
+	gen_tcp:close(Sock).
